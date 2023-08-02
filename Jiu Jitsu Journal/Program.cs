e799afc701f68d8c3ebdcdcda5cd6d496b345cf7
Aaron Dean <adean30462@gmail.com>
2023-08-02 15:44:15 -0400

    anotha one

--- Jiu Jitsu Journal/Program.cs
+++ Jiu Jitsu Journal/Program.cs
@@ -7,7 +7,8 @@ namespace Jiu_Jitsu_Journal
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            builder.Services.AddTransient<IUserProfileRepository, UserProfileRepository>(); 
+            builder.Services.AddTransient<IClassRepository, ClassRepository>();
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
